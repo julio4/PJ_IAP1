2d7fc276ae5e9a3a597c2fb1dcbc054a3dd4d6e2
Jules Doumèche <30329843+julio4@users.noreply.github.com>
2019-09-28 14:18:12 +0200

    main.c base

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+/* FONCTIONS */
+
+int exit() {
+
+}
+
+int definir_parcours() {
+
+}
+
+int inscrire_equipe() {
+
+}
+
+int afficher_equipe() {
+
+}
+
+int enregistrer_temps() {
+
+}
+
+int detection_fin_poursuite() {
+
+}
+
+int detection_fin_competition() {
+
+}
+
+int afficher_temps() {
+
+}
+
+int afficher_temps_equipe() {
+
+}
+
+int main() {
+	while 1 {
+
+	}
+}
