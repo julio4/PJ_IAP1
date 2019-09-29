5405ce87467b8dd328f51812c9dcf9b43f59cf19
Jules Doumèche <30329843+julio4@users.noreply.github.com>
2019-09-29 10:46:31 +0200

    main.c
    bug

--- ProjetIAP1/PIAP1/main.c
+++ ProjetIAP1/PIAP1/main.c
@@ -5,7 +5,7 @@
 #include <string.h>
 
 /* FONCTIONS */
-
+/*
 int definir_parcours() {
 	return 1;
 }
@@ -37,12 +37,17 @@ int afficher_temps() {
 int afficher_temps_equipe() {
 	return 1;
 }
+*/
 
 int main() {
 	int on = 1;
 	int t = 0;
 	int n = 0;
+	int nb_equipes = 0;
+	int dossart = 101;
+	char no_dossart[20];
 	char stop = ' ';
+	char affichage = ' ';
 	while (on) {
 		//definir_parcours 2 
 		scanf("definir_parcours %d", &t);
@@ -52,12 +57,37 @@ int main() {
 		//definir_nombre_épreuves 2
 		scanf("definir_nombre_épreuves %d", &n);
 
+		char equipes[2][7] = ""; //Par equipe : 1 Pays, 3 Noms + 3 dossarts
+		nb_equipes = 2;
+
+		//inscrire_equipe Canada Blondin Weidemann Morrison
+		for (int i = 0; i < 2; ++i) {
+			scanf("inscrire_equipe %s %s %s %s", &equipes[0][0], &equipes[0][1], &equipes[0][3], &equipes[0][5]);
+			for (int j = 1; j < 5; ++j) {
+				sprintf(no_dossart, "%d", dossart); // convertit int dossart en char no_dossart
+				equipes[i][j + 1] = no_dossart; //mais sa marche pas
+				printf("Inscription dossart %d\n", dossart);
+				++dossart;
+			}
+			++nb_equipes;
+		}
+
+		//afficher_equipes
+		scanf("afficher_equipe%c", &affichage);
+		if (affichage == 's') {
+			for (int i = 0; i < nb_equipes - 1; ++i) {
+				printf("%s", equipes[i][0]); // Pays
+				for (int j = 1; j < 5; ++j) {
+					printf("%s %c", equipes[i][j], equipes[i][j + 1]); //Noms + dossarts
+				}
+			}
+		}
+
 		//exit
-		//scanf("exi%c", &stop);
-		//if (stop == 't') {
-		//	on = 0;
-		//}
-		on = 0;
+		scanf("exi%c", &stop);
+		if (stop == 't') {
+			on = 0;
+		}
 	}
 	return 0;
 }
