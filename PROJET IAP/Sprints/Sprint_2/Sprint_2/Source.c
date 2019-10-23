beaf9f007a2514cf5da0bc380029dd061c08e7e7
LightBowFire <55929788+LightBowFire@users.noreply.github.com>
2019-10-23 14:06:24 +0200

    Add files via upload

--- PROJET IAP/Sprints/Sprint_2/Sprint_2/Source.c
+++ PROJET IAP/Sprints/Sprint_2/Sprint_2/Source.c
@@ -1,7 +1,7 @@
 /*              Projet 1 IAP 2019              */
-/* 	                Sprint 1                   */
+/* 	                Sprint 2                   */
 /*Jules DOUMECHE et Gwénolé MARTIN - Groupe 111*/
-/*     Fin de développement le 17/10/2019      */
+/*     Fin de développement le 18/10/2019      */
 
 #pragma warning(disable:4996)
 #pragma warning(disable:6031)
@@ -22,6 +22,8 @@ enum INIT { PREMIER_DOSSARD = 101 - 1 };															// Constantes d'initialis
 typedef struct {
 	char nom[MAX_MOT + 1];																			//Nom du patineur
 	unsigned int dossard;																			//Numéro de dossard du patineur
+	unsigned int tour;
+	double temps[MAX_TOURS];
 }Patineur;																							//^^ Structure patineur ^^//
 
 
@@ -49,6 +51,8 @@ void definir_parcours(int* parcours);
 void definir_nombre_epreuves(int* epreuves);
 void inscrire_equipe(Inscrits* ins);
 void afficher_equipes(const Inscrits* ins);
+void enregistrer_temps(Course* course);
+void afficher_temps(const Course* course);
 
 
 /* MAIN */
@@ -61,9 +65,9 @@ int main() {
 	int epreuves = 0;
 	Inscrits ins = { .nbInscrits = 0 };
 
-	while (1) { 
+	while (1) {
 
-		scanf("%s", mot); 
+		scanf("%s", mot);
 
 		if (strcmp(mot, "definir_parcours") == 0) {
 			definir_parcours(&parcours);
@@ -81,13 +85,21 @@ int main() {
 			afficher_equipes(&ins);
 		}
 
+		else if (strcmp(mot, "enregistrer_temps") == 0) {
+			enregistrer_temps(&ins.course);
+		}
+
+		else if (strcmp(mot, "afficher_temps") == 0) {
+			afficher_temps(&ins.course);
+		}
+
 		else if (strcmp(mot, "exit") == 0) {
-			exit(0); 
+			exit(0);
 		}
 	}
 }
 
-/* Enregistre le nombre de "parcours" 
+/* Enregistre le nombre de "parcours"
  * [in] entier parcours
  * [out] pas de out, changement de la valeur de "parcours" à l'aide d'un pointeur
  */
@@ -110,11 +122,11 @@ void definir_nombre_epreuves(int* epreuves) {
 void inscrire_equipe(Inscrits* ins) {
 
 	int nb_eq_inscrites = (ins->nbInscrits) / MAX_EQUIPIERS;
-	static int dernier_dossard = PREMIER_DOSSARD; 
+	static int dernier_dossard = PREMIER_DOSSARD;
 
-	scanf("%s", ins->course->equipe[nb_eq_inscrites].pays); 
+	scanf("%s", ins->course->equipe[nb_eq_inscrites].pays);
 
-	for (int i = 0; i < MAX_EQUIPIERS; ++i) { 
+	for (int i = 0; i < MAX_EQUIPIERS; ++i) {
 
 		scanf("%s", ins->course->equipe[nb_eq_inscrites].dataPatineurs[i].nom);
 		dernier_dossard++;
@@ -134,11 +146,11 @@ void afficher_equipes(const Inscrits* ins) {
 
 	int nb_eq_inscrites = (ins->nbInscrits) / MAX_EQUIPIERS;
 
-	for (int i = 0; i < nb_eq_inscrites; ++i) { 
+	for (int i = 0; i < nb_eq_inscrites; ++i) {
 
 		printf("%s ", ins->course->equipe[i].pays);
 
-		for (int j = 0; j < MAX_EQUIPIERS; ++j) { 
+		for (int j = 0; j < MAX_EQUIPIERS; ++j) {
 
 			printf("%s ", ins->course->equipe[i].dataPatineurs[j].nom);
 			printf("%d", ins->course->equipe[i].dataPatineurs[j].dossard);
@@ -153,5 +165,43 @@ void afficher_equipes(const Inscrits* ins) {
 
 		printf("\n\n");
 
+	}
+}
+
+
+/* Enregistre un temps pour un dossard et un tour donné
+ * [in] Course course
+ * [out] pas de out, enregistre le temps en fonction du dossard et du tour
+ */
+void enregistrer_temps(Course* course) {
+	//calcul avec le dossard pour retrouver le bon patineur
+	int dossard;
+	scanf("%d", &dossard);
+	int numero_equipe = (dossard - 101) / 3;
+	int numero_joueur = ((dossard - 101) % 3);
+
+	//enregistrement du numéro du tour
+	scanf("%d", &course->equipe[numero_equipe].dataPatineurs[numero_joueur].tour);
+
+	//enregistrement du temps
+	scanf("%lf", &course->equipe[numero_equipe].dataPatineurs[numero_joueur].temps[(course->equipe[numero_equipe].dataPatineurs[numero_joueur].tour) - 1]);
+}
+
+
+/* Affiche le temps d'un Patineur
+ * [in] Course course (en constante pour l'affichage)
+ * [out] pas de out, affiche le temps ou les temps (pour chaque tour) d'un patineur en fonction de son numéro de dossard
+ */
+void afficher_temps(const Course* course) {
+	int dossard;
+	scanf("%d", &dossard);
+	int numero_equipe = (dossard - 101) / 3;
+	int numero_joueur = ((dossard - 101) % 3);
+
+	for (unsigned int i = 0; i < (course->equipe[numero_equipe].dataPatineurs[numero_joueur].tour); ++i) {
+		printf("%s ", course->equipe[numero_equipe].pays);
+		printf("%d ", (i + 1));
+		printf("%s ", course->equipe[numero_equipe].dataPatineurs[numero_joueur].nom);
+		printf("%.1f\n\n", course->equipe[numero_equipe].dataPatineurs[numero_joueur].temps[i]);
 	}
 }
--- PROJET IAP/Sprints/Sprint_3/Sprint_3/Source.c
+++ PROJET IAP/Sprints/Sprint_3/Sprint_3/Source.c
@@ -1,7 +1,7 @@
 /*              Projet 1 IAP 2019              */
-/* 	                Sprint 1                   */
+/* 	                Sprint 3                   */
 /*Jules DOUMECHE et Gwénolé MARTIN - Groupe 111*/
-/*     Fin de développement le 17/10/2019      */
+/*     Fin de développement le 18/10/2019      */
 
 #pragma warning(disable:4996)
 #pragma warning(disable:6031)
@@ -22,6 +22,8 @@ enum INIT { PREMIER_DOSSARD = 101 - 1 };															// Constantes d'initialis
 typedef struct {
 	char nom[MAX_MOT + 1];																			//Nom du patineur
 	unsigned int dossard;																			//Numéro de dossard du patineur
+	unsigned int tour;																				//Tour du patineur
+	double temps[MAX_TOURS];																		//Temps du patineur (pour chaque tour)
 }Patineur;																							//^^ Structure patineur ^^//
 
 
@@ -49,6 +51,9 @@ void definir_parcours(int* parcours);
 void definir_nombre_epreuves(int* epreuves);
 void inscrire_equipe(Inscrits* ins);
 void afficher_equipes(const Inscrits* ins);
+void enregistrer_temps(Course* course);
+void afficher_temps(const Course* course);
+void afficher_temps_equipes(Inscrits* ins);
 
 
 /* MAIN */
@@ -61,9 +66,9 @@ int main() {
 	int epreuves = 0;
 	Inscrits ins = { .nbInscrits = 0 };
 
-	while (1) { 
+	while (1) {
 
-		scanf("%s", mot); 
+		scanf("%s", mot);
 
 		if (strcmp(mot, "definir_parcours") == 0) {
 			definir_parcours(&parcours);
@@ -81,13 +86,25 @@ int main() {
 			afficher_equipes(&ins);
 		}
 
+		else if (strcmp(mot, "enregistrer_temps") == 0) {
+			enregistrer_temps(&ins.course);
+		}
+
+		else if (strcmp(mot, "afficher_temps") == 0) {
+			afficher_temps(&ins.course);
+		}
+
+		else if (strcmp(mot, "afficher_temps_equipes") == 0) {
+			afficher_temps_equipes(&ins);
+		}
+
 		else if (strcmp(mot, "exit") == 0) {
-			exit(0); 
+			exit(0);
 		}
 	}
 }
 
-/* Enregistre le nombre de "parcours" 
+/* Enregistre le nombre de "parcours"
  * [in] entier parcours
  * [out] pas de out, changement de la valeur de "parcours" à l'aide d'un pointeur
  */
@@ -110,11 +127,11 @@ void definir_nombre_epreuves(int* epreuves) {
 void inscrire_equipe(Inscrits* ins) {
 
 	int nb_eq_inscrites = (ins->nbInscrits) / MAX_EQUIPIERS;
-	static int dernier_dossard = PREMIER_DOSSARD; 
+	static int dernier_dossard = PREMIER_DOSSARD;
 
-	scanf("%s", ins->course->equipe[nb_eq_inscrites].pays); 
+	scanf("%s", ins->course->equipe[nb_eq_inscrites].pays);
 
-	for (int i = 0; i < MAX_EQUIPIERS; ++i) { 
+	for (int i = 0; i < MAX_EQUIPIERS; ++i) {
 
 		scanf("%s", ins->course->equipe[nb_eq_inscrites].dataPatineurs[i].nom);
 		dernier_dossard++;
@@ -134,11 +151,11 @@ void afficher_equipes(const Inscrits* ins) {
 
 	int nb_eq_inscrites = (ins->nbInscrits) / MAX_EQUIPIERS;
 
-	for (int i = 0; i < nb_eq_inscrites; ++i) { 
+	for (int i = 0; i < nb_eq_inscrites; ++i) {
 
 		printf("%s ", ins->course->equipe[i].pays);
 
-		for (int j = 0; j < MAX_EQUIPIERS; ++j) { 
+		for (int j = 0; j < MAX_EQUIPIERS; ++j) {
 
 			printf("%s ", ins->course->equipe[i].dataPatineurs[j].nom);
 			printf("%d", ins->course->equipe[i].dataPatineurs[j].dossard);
@@ -153,5 +170,81 @@ void afficher_equipes(const Inscrits* ins) {
 
 		printf("\n\n");
 
+	}
+}
+
+
+/* Enregistre un temps pour un dossard et un tour donné
+ * [in] Course course
+ * [out] pas de out, enregistre le temps en fonction du dossard et du tour
+ */
+void enregistrer_temps(Course* course) {
+	//calcul avec le dossard pour retrouver le bon patineur
+	int dossard;
+	scanf("%d", &dossard);
+	int numero_equipe = (dossard - 101) / 3;
+	int numero_joueur = ((dossard - 101) % 3);
+
+	//enregistrement du numéro du tour
+	scanf("%d", &course->equipe[numero_equipe].dataPatineurs[numero_joueur].tour);
+
+	//enregistrement du temps
+	scanf("%lf", &course->equipe[numero_equipe].dataPatineurs[numero_joueur].temps[(course->equipe[numero_equipe].dataPatineurs[numero_joueur].tour) - 1]);
+}
+
+
+/* Affiche le temps d'un Patineur
+ * [in] Course course (en constante pour l'affichage)
+ * [out] pas de out, affiche le pays et le temps ou les temps (pour chaque tour) d'un patineur en fonction de son numéro de dossard
+ */
+void afficher_temps(const Course* course) {
+	int dossard;
+	scanf("%d", &dossard);
+	int numero_equipe = (dossard - 101) / 3;
+	int numero_joueur = ((dossard - 101) % 3);
+
+	for (unsigned int i = 0; i < (course->equipe[numero_equipe].dataPatineurs[numero_joueur].tour); ++i) {
+		printf("%s ", course->equipe[numero_equipe].pays);
+		printf("%d ", (i + 1));
+		printf("%s ", course->equipe[numero_equipe].dataPatineurs[numero_joueur].nom);
+		printf("%.1f\n\n", course->equipe[numero_equipe].dataPatineurs[numero_joueur].temps[i]);
+	}
+}
+
+
+/* Affiche le temps d'une équipe de Patineurs
+ * [in] Inscrits ins (en constante pour l'affichage)
+ * [out] pas de out, affiche le pays et le temps du dernier patineur de chaque équipe (temps le plus grand)
+ */
+void afficher_temps_equipes(const Inscrits* ins) {
+
+	int tour;
+	scanf("%d", &tour);
+	--tour; // décrémente tour pour la position dans le tableau temps
+
+	int nb_eq_inscrites = (ins->nbInscrits) / MAX_EQUIPIERS;
+
+	for (int i = 0; i < nb_eq_inscrites; ++i) {
+		double a = ins->course->equipe[i].dataPatineurs[0].temps[tour]; //initialise a, b, et c pour éviter les longues lignes de structures dans les conditions
+		double b = ins->course->equipe[i].dataPatineurs[1].temps[tour]; 
+		double c = ins->course->equipe[i].dataPatineurs[2].temps[tour]; 
+		if (a > 0.0 && b > 0.0 && c > 0.0) {
+			printf("%s ", ins->course->equipe[i].pays);
+			if (a < b || a < c) {
+				if (b < c) {
+					printf("%.1f", c);
+				}
+				else {
+					printf_s("%.1f", b);
+				}
+			}
+			else {
+				printf_s("%.1f", a);
+			}
+		}
+		else {
+			printf("indisponible");
+		}
+		printf("\n\n");
 	}
 }
